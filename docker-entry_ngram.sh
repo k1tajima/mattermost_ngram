f53e022cb9e3ab67d1aa9f8be5ec60fb18a0ddef
tajima <tajima@dcinc.co.jp>
2018-06-02 19:16:35 +0900

    bugfix

--- docker-entry_ngram.sh
+++ docker-entry_ngram.sh
@@ -6,7 +6,7 @@ echo "Activate N-gram parser on MySQL."
 echo "DEBUG: mysql -user=$MYSQL_USER -password=$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF"
 mysql -user="$MYSQL_USER" -password="$MYSQL_PASSWORD" "$MYSQL_DATABASE" <<EOF
 ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
-ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (`Message`) WITH PARSER ngram COMMENT 'ngram reindex';
+ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
 EOF
 
 # Entrypoint for mattermost-preview
