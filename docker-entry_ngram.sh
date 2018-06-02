b0b5200c0d62177aeb79a688cbcdd77251495068
tajima <tajima@dcinc.co.jp>
2018-06-02 19:39:56 +0900

    debug

--- docker-entry_ngram.sh
+++ docker-entry_ngram.sh
@@ -4,10 +4,10 @@ set -eu
 # Use N-gram parser on MySQL to search a sentence in Japanese.
 echo "Activate N-gram parser on MySQL."
 echo "DEBUG: mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF"
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF
-ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
-ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
-EOF
+# mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF
+# ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
+# ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
+# EOF
 
 # Entrypoint for mattermost-preview
 # ref=https://github.com/mattermost/mattermost-docker-preview/blob/master/Dockerfile
