73b05f1870e045c82ab73d9a04a3119c4525312a
tajima <tajima@dcinc.co.jp>
2018-06-03 02:45:47 +0900

    debug

--- docker-entry_ngram.sh
+++ docker-entry_ngram.sh
@@ -29,4 +29,5 @@ ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
 ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
 EOF
 
+fg
 exec "$@"
