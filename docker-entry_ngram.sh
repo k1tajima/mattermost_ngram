6e2e51b311442d3bc78d121ad7d7d28e566f3d1e
tajima <tajima@dcinc.co.jp>
2018-06-02 20:17:12 +0900

    mattermost-preview の entry.sh をベースに変更。

--- docker-entry_ngram.sh
+++ docker-entry_ngram.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
+# ref=https://github.com/mattermost/mattermost-docker-preview/blob/master/docker-entry.sh
 set -eu
+cd "$(dirname $0)"
+echo "DEBUG: pwd=$PWD"
+
+# Run MySQL
+echo "Starting MySQL"
+/entrypoint.sh mysqld &
+
+until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processlist &> /dev/null; do
+	echo "MySQL still not ready, sleeping"
+	sleep 5
+done
 
 # Use N-gram parser on MySQL to search a sentence in Japanese.
 echo "Activate N-gram parser on MySQL."
-echo "DEBUG: mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF"
-# mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF
-# ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
-# ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
-# EOF
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DATABASE <<EOF
+ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
+ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
+EOF
 
-# Entrypoint for mattermost-preview
-# ref=https://github.com/mattermost/mattermost-docker-preview/blob/master/Dockerfile
-echo "DEBUG: pwd=$PWD"
-exec "$(dirname $0)/docker-entry.sh" "$@"
+echo "Starting platform"
+cd mattermost
+exec ./bin/platform --config=config/config_docker.json "$@"
