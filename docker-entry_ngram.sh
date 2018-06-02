3d0c0fa3643439c71ef8e76454563f519538bac9
tajima <tajima@dcinc.co.jp>
2018-06-02 19:32:20 +0900

    debug

--- docker-entry_ngram.sh
+++ docker-entry_ngram.sh
@@ -3,8 +3,8 @@ set -eu
 
 # Use N-gram parser on MySQL to search a sentence in Japanese.
 echo "Activate N-gram parser on MySQL."
-echo "DEBUG: mysql -user=$MYSQL_USER -password=$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF"
-mysql -user=$MYSQL_USER -password=$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF
+echo "DEBUG: mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF"
+mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE <<EOF
 ALTER TABLE Posts DROP INDEX idx_posts_message_txt;
 ALTER TABLE Posts ADD FULLTEXT INDEX idx_posts_message_txt (\`Message\`) WITH PARSER ngram COMMENT 'ngram reindex';
 EOF
